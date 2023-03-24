52ad8ab444ff6ce222e6e81a6eafd26bcd7e23b3
Luca Soda <luca.soda@Lucas-Laptop.wind3.hub>
2023-03-24 16:19:55 +0100

    setup-strapi-docker

--- lib/StrapiHandler.test.ts
+++ lib/StrapiHandler.test.ts
@@ -4,7 +4,7 @@ import { FilterOperator, SortDirection } from './Interfaces';
 
 const uuidv4 = uuidLib.v4;
 
-const strapiUrl = 'http://127.0.0.1:1337'
+const strapiUrl = 'http://127.0.0.1:7125'
 const apiKey = '37a9dca00ffbd809de068adf97ae7b4bcc0b78a3102798fd393d7126da8984fbf563e6ebf6e507e3fc93970b9971ecee6175096e85523169d6feea6f89d2d15ccafcf6ad4e7adcab5c99c6146e959a4106e92c619933a050d558f692afaf2a703ad55c151ebf90485fab1e7a61f035b722177cec37f9037ee3292d8e2c1a160b';
 const collectionName = 'Tests';
 const relationOne = 'Relation-ones';
---
+++ strapi/src/api/relation-one/services/relation-one.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * relation-one service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::relation-one.relation-one');
