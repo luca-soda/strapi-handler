23e5bf3b46dbc74a81db2dacccc3d5a10befab78
Luca Soda <luca.soda@Lucas-Laptop.wind3.hub>
2023-03-28 11:44:36 +0200

    Update export with interfaces

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { Filter, FilterOperator, OptionalParams, SortDirection } from "./lib/Interfaces";
 import StrapiHandler from "./lib/StrapiHandler";
 
 export default StrapiHandler;
+export { FilterOperator, OptionalParams, SortDirection, Filter }
